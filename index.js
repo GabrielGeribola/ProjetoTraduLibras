71597b0f4b41519617261b0b5c630518295c93f1
bielgeri <geribolarodrigues@gmail.com>
2024-04-16 15:33:34 -0300

    Implementações de alguns dados mysql
    Ainda falta inserir a data base e fazer o teste de validção do BD.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 const express = require('express');
 const app = express();
+
 
 const db = require('./src/models/db')
 
--- src/models/db.js
+++ src/models/db.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('inserir database', 'inserir usuario', 'inserir senha', {
+const sequelize = new Sequelize('inserir database', 'tradulibras_adm', 'Jv337299', {
   host: 'localhost',
-  dialect:  'mssql'
+  dialect:  'mysql'
 });
 
 
 //Validar se a conexão do BD está funcionando.
 sequelize.authenticate()
-.then(function() {
+.then(() => {
   console.log("Conexão com banco de dados realizada com sucesso.")
 }).catch(e => {
   console.log("Erro: Conexão com o banco de dados não realizada com sucesso.")
