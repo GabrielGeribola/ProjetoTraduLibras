8e415e4dfe076404a1706140afc1ea1433fb0f11
bielgeri <geribolarodrigues@gmail.com>
2024-04-17 11:17:43 -0300

    Mudanças de rotas e modelo ejs
    Implementei modelo MVC (Model, View, Controller), com um arquivo ejs sendo ele o front end a ser implementado futuramente, e um arquivo Routes.js para gerenciar as rotas de acessos do aplicativo

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 
 const express = require('express');
 const app = express();
-
+const path = require('path');
+const routes = require('./routes');
 
 const db = require('./src/models/db')
 
-app.get("/", async (req, res) => {
-  res.send("Página Inicial- database")
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+app.use(routes);
+
+app.get("/teste", async (req, res) => {
+  res.send('Database')
 });
 
 app.listen(3001, () => {
-  console.log("Servidor iniciado na porta 8080: http://localhost:3001")
+  console.log("Servidor iniciado na porta 3001: http://localhost:3001")
 });
