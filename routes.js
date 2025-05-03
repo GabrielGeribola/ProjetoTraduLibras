f512687d3a6216b913807f6a16ed44af01e7dda0
Catarina Dalsan <catarina.dalsan.ribeiro@gmail.com>
2025-05-02 23:11:14 -0300

    Fiz a pagina cursos, nao finalizada

--- routes.js
+++ routes.js
@@ -3,11 +3,14 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 
-//Rotas da home
+// Rotas da home
 route.get('/', homeController.index);
 route.get('/home', homeController.home);
 
-//rotas de login
+// Rota para a página de cursos
+route.get('/cursos', homeController.cursos); // Adicionando a rota de cursos
+
+// Rotas de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
@@ -15,11 +18,8 @@ route.get('/login/logout', loginController.logout);
 route.get('/register', loginController.registerPage);
 
 // Rotas de tradução
-//route.get('/translate', (req, res) => res.render('home', { originalText: '', translatedText: '' }));
-//route.post('/translate' , translateController.transaleAndSave)
-
-//route.post('/translate', translateController.translate);
-
-
+// route.get('/translate', (req, res) => res.render('home', { originalText: '', translatedText: '' }));
+// route.post('/translate', translateController.transaleAndSave);
+// route.post('/translate', translateController.translate);
 
 module.exports = route;
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,14 @@
+// Rota para a página inicial (index)
 exports.index = (req, res) => {
   res.render('index');
 };
 
+// Rota para a página home
 exports.home = (req, res) => {
   res.render('home', {originalText: '', translatedText: ''});
 };
 
-
-
-
+// Rota para a página de cursos
+exports.cursos = (req, res) => {
+  res.render('cursos'); // Renderiza o arquivo cursos.ejs
+};
